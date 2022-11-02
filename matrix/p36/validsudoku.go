ada6f203449a40730e02b96013cee2dad9cbdd87
Bartlomiej Wilczynski <me@bwilczynski.com>
2022-11-02 16:55:24 +0100

    36. Valid Sudoku

---
+++ matrix/p36/validsudoku.go
@@ -0,0 +1,39 @@
+package p36
+
+func isValidSudoku(board [][]byte) bool {
+	rows := make([]map[byte]struct{}, len(board))
+	cols := make([]map[byte]struct{}, len(board))
+	boxes := make([]map[byte]struct{}, len(board))
+
+	boxIndex := func(i, j int) int {
+		return 3*(i/3) + j/3
+	}
+
+	for i := 0; i < len(board); i++ {
+		rows[i] = make(map[byte]struct{})
+		cols[i] = make(map[byte]struct{})
+		boxes[i] = make(map[byte]struct{})
+	}
+	for i, row := range board {
+		rows[i] = make(map[byte]struct{})
+		for j, val := range row {
+			if val == '.' {
+				continue
+			}
+			if _, ok := rows[i][val]; ok {
+				return false
+			}
+			if _, ok := cols[j][val]; ok {
+				return false
+			}
+			k := boxIndex(i, j)
+			if _, ok := boxes[k][val]; ok {
+				return false
+			}
+			rows[i][val] = struct{}{}
+			cols[j][val] = struct{}{}
+			boxes[k][val] = struct{}{}
+		}
+	}
+	return true
+}
